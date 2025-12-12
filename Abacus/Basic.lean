2359955d4ddced0623c02cd763355b4cf744104c
Jelle Wemmenhove <a.j.wemmenhove@tue.nl>
2025-12-12 10:25:09 +0100

    More structure MaybeUndefined. Progress proof  iff .

--- Abacus/Basic.lean
+++ Abacus/Basic.lean
@@ -3,7 +3,51 @@ import Mathlib.Data.EReal.Inv
 import Mathlib.Data.EReal.Operations
 
 
+def MaybeUndefined (α : Type*) := Set α
+/- `Set α` instead of `α → Prop` because this gives lots of nice infrastructure for free,
+such as `Set.singleton` and `Set.map2` -/
 
+-- notation
+postfix:max "??" => MaybeUndefined
+
+namespace MaybeUndefined
+
+@[ext]
+theorem ext {α : Type*} {P Q : MaybeUndefined α} (h : ∀ x : α, P x ↔ Q x) : P = Q :=
+  funext (fun x ↦ propext (h x))
+
+def mk {α : Type*} (P : α → Prop) : MaybeUndefined α := P
+def of_defined {α : Type*} (x : α) : MaybeUndefined α := Set.singleton x
+
+instance {α : Type*} : Coe α (MaybeUndefined α) where
+  coe := of_defined
+
+
+/- Establish key properties of `MaybeUndefined α`
+(theorem names can be improved) -/
+
+theorem satisfies_of_eq_defined {α : Type} {P : α → Prop} {x : α}
+  (h : mk P = x) : P x := by
+  unfold mk of_defined Set.singleton at h
+  rw [h]
+  rfl
+
+theorem unique_satisfies_of_eq_defined {α : Type} {P : α → Prop} {x y : α}
+  (h : mk P = x) (hy : P y) : y = x := by
+  unfold mk of_defined Set.singleton at h
+  rwa [h] at hy
+
+theorem eq_defined_of_unique_satisfies_of_satisfies {α : Type} {P : α → Prop} {x : α}
+  (hx : P x) (hunique : ∀ {y z}, P y → P z → y = z) : mk P = x := by
+  ext y
+  constructor <;> intro hy
+  · exact hunique hy hx
+  · rwa [hy]
+
+end MaybeUndefined
+
+
+-- Code to make `|x|` notation possible for EReals
 @[inherit_doc abs]
 macro:max atomic("|" noWs) a:term noWs "|" : term => `(EReal.abs $a)
 
@@ -18,20 +62,10 @@ macro:max atomic("|" noWs) a:term noWs "|" : term => `(EReal.abs $a)
 --   | _ => throw ()
 
 
-open EReal
-
 notation "Number" => EReal
 -- Maybe make stucture or definition (how much unfolding is done automatically?)
-
-def MaybeUndefined (α : Type*) := (α → Prop)
-postfix:max "??"  => MaybeUndefined
-
-def MaybeUndefined.mk {α : Type*} (P : α → Prop) : MaybeUndefined α := P
-def MaybeUndefined.defined {α : Type*} (x : α) : MaybeUndefined α := (fun y : α ↦ (y = x))
-
-instance {α : Type*}: Coe α (MaybeUndefined α) where
-  coe := MaybeUndefined.defined
-
+notation "∞" => (⊤ : Number)
+-- notation "-∞" => (⊥ : Number)   -- TODO: shoul be print only notation
 
 variable (y : Number)
 #check forall x : ℝ, x = y
@@ -46,16 +80,26 @@ example : ∀ A ⊆ RealNumber, Nonempty A := sorry
 def convergence_seq (a : Number → Number) (p : Number) : Prop :=
   ∀ ε ∈ RealNumber, ε > 0 → ∃ N ∈ NatNumber, ∀ n ∈ NatNumber, n ≥ N → |a n - p| < ε
 
-#eval |(⊤ : EReal) - ⊤|
-#eval |(⊥ : EReal) - ⊥|
+example :  (∞ : EReal) - ∞  = -∞ := rfl
+example : |(∞ : EReal) - ∞| =  ∞ := rfl
+example : (-∞ : EReal) + ∞  = -∞ := rfl
 
 def lim_seq (a : Number → Number) :=
   MaybeUndefined.mk (convergence_seq a)
 
-example (a : Number → Number) (p : Number) : (lim_seq a = p) ↔ (convergence_seq a p) := by
-  unfold lim_seq
-  unfold MaybeUndefined.defined
-  unfold MaybeUndefined.mk
-  constructor <;> intro h
-  · rw [h]
-  · ext p' -- ext lemma maken en registreren
+
+theorem unique_convergence_seq {a : Number → Number} {p₁ p₂ : Number} (hp₁ : convergence_seq a p₁)
+  (hp₂ : convergence_seq a p₂) : p₁ = p₂ := by
+  by_contra
+  -- work, also because of ∞ and -∞
+  -- maybe link to results from MathLib? Filter.Tendsto g b (nhds a))
+  #check Topology.Hausdorff.tendsto_nhds_unique
+  sorry
+
+theorem lim_seq_iff_convergent_seq (a : Number → Number) (p : Number) :
+  (lim_seq a = p) ↔ (convergence_seq a p) := by
+  constructor
+  · apply MaybeUndefined.satisfies_of_eq_defined
+  · intro hp
+    apply MaybeUndefined.eq_defined_of_unique_satisfies_of_satisfies hp
+    exact unique_convergence_seq
