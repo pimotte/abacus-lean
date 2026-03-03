da2ab1817c8e4b3166b571ffcb4cffd7e4cf6e76
Jelle Wemmenhove <a.j.wemmenhove@tue.nl>
2026-03-03 15:52:38 +0100

    Quick save session with Jim

--- Abacus/Limit.lean
+++ Abacus/Limit.lean
@@ -662,19 +662,73 @@ end Laws
 
 section Example
 
--- theorem lim_seq_divn_zero : lim_seq (fun n ↦ 1/n) = 0 := by sorry
-theorem lim_seq_divn_zero : lim_seq (fun n ↦ 1/n) = MaybeUndefined.of_def 0 := by sorry
+
 
 open WithBotTop
 #check MaybeUndefined.of_def (∞ : [-∞,∞]) + MaybeUndefined.of_def (∞ : [-∞,∞])
 
-lemma computation₁ : lim_seq (fun n ↦ 1 + 1/n) = MaybeUndefined.of_def 1 := by
+
+instance : Top (MaybeUndefined [-∞,∞]) where
+  top := MaybeUndefined.of_def (⊤ : [-∞,∞])
+instance : Bot (MaybeUndefined [-∞,∞]) where
+  bot := MaybeUndefined.of_def (⊥ : [-∞,∞])
+
+instance {n : Nat} : OfNat (MaybeUndefined [-∞,∞]) n where
+  ofNat := MaybeUndefined.of_def (some (some (Real.instNatCast.natCast n)))
+
+@[default_instance 501]
+instance : OfScientific (MaybeUndefined [-∞,∞]) where
+  ofScientific := by
+    have f : OfScientific Number := by infer_instance
+    exact (fun mantissa exponent decExponent ↦
+      MaybeUndefined.of_def (f.ofScientific mantissa exponent decExponent))
+
+-- by infer_instance
+
+#check (∞ : [-∞,∞])
+#check (∞ : MaybeUndefined [-∞,∞])
+#check (1 : MaybeUndefined [-∞,∞])
+
+example : MaybeUndefined.of_def (2 : [-∞,∞]) = 2 := by
+  rfl
+
+example : MaybeUndefined.of_def (0 : [-∞,∞]) = 0 := by
+  rw [← Nat.cast_zero]
+  rfl
+
+-- theorem lim_seq_divn_zero : lim_seq (fun n ↦ 1/n) = 0 := by sorry
+theorem lim_seq_divn_zero : lim_seq (fun n ↦ 1/n) = 0 := by sorry
+
+lemma computation₁ : lim_seq (fun n ↦ 1 + 1/n) = 1 := by
   calc
     lim_seq (fun n ↦ 1 + 1/n)
       = lim_seq (fun n ↦ 1) + lim_seq (fun n ↦ 1/n) := by sorry
     _ = 1 + lim_seq (fun n ↦ 1/n) := by sorry
     _ = 1 + 0 := by rw [lim_seq_divn_zero]
-    _ = 1 := by sorry
+    _ = 1 := by
+      calc
+        (1 : MaybeUndefined [-∞,∞]) + 0 = MaybeUndefined.of_def 1 + MaybeUndefined.of_def 0 := by
+              rw [← Nat.cast_one, ← Nat.cast_zero]; rfl
+        _ = MaybeUndefined.of_def (1 + 0) := by
+              rfl
+        _ = MaybeUndefined.of_def 1 := by norm_num
+        _ = 1 := by rw [← Nat.cast_one]; rfl
+
+        -- norm_cast, norm_fun (push coercions etc.)
+
+example : lim_seq (fun n ↦ (2 - 1)/n) = 0 := by
+  calc
+    lim_seq (fun n ↦ (2 - 1)/n)
+      = lim_seq (fun n ↦ 1/n) := by norm_num
+    _ = 0 := by rw [lim_seq_divn_zero]
+
+
+example {a : Number → Number} (ha : ∀ n, a n = 1/n) : lim_seq (fun n ↦ a n) = 0 := by
+  calc
+    lim_seq (fun n ↦ a n)
+      = lim_seq (fun n ↦ 1/n) := by simp_rw [ha]
+    _ = 0 := by rw [lim_seq_divn_zero]
+
 
 lemma corollary₁ : myTendsto (fun n ↦ 1 + 1/n) NatNumber ∞ 1 :=
   MaybeUndefined.satisfies_of_eq_defined computation₁
